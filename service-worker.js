1f241d97cac99fe18f6eaa34754b561407abb8a0
pawel54321 <pawlo.pw36@gmail.com>
2019-02-22 11:36:54 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.6e61fb454005f7ac825a61a372d52d0d.js"
+  "/home/precache-manifest.5bec2211d524f67ff0025eb5a09672df.js"
 );
 
 workbox.clientsClaim();
